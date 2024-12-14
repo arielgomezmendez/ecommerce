7b771acadc366c810082c10933d7a9583202e920
Ariel <arielgomezmendez94@gmail.com>
2024-12-13 21:53:13 -0500

    Create nabvar component

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -6,6 +6,7 @@ import MyOrder from "../MyOrder"
 import MyOrders from "../MyOrders"
 import NotFound from "../NotFound"
 import SignIn from "../SignIn"
+import Navbar from "../../Components/Navbar"
 
 const AppRoutes = () => {
   const routes = useRoutes([
@@ -24,6 +25,7 @@ function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <Navbar/>
     </BrowserRouter>
   )
 }
