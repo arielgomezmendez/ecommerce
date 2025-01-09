bc1cda4ead200d6a4c46a48d277cca051f4c421a
Ariel <arielgomezmendez94@gmail.com>
2025-01-09 15:45:31 -0500

    Set styles of orders cards

--- src/Pages/MyOrders/index.tsx
+++ src/Pages/MyOrders/index.tsx
@@ -8,12 +8,10 @@ const MyOrders = (): JSX.Element => {
 
   const context = useContext(ShoppingCartContext);
 
-  
-
   return (
     <Layout>
-      <div className="flex w-80 items-center relative justify-center">
-        <h1>My Orders</h1>
+      <div className="flex w-80 items-center relative justify-center mb-3">
+        <h1 className="font-medium text-xl">My Orders</h1>
       </div>
      
       {context.order.map((order, index) => (
